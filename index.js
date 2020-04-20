9afd2113bedc96d255824e52c1b945a1f7f7043e
Mikko Kurkela <mikko.sahkoposti@gmail.com>
2020-04-20 19:53:10 +0300

    Firebase functions to add user and remove user (#8)
    * Initial add user and delete user cloud functions implementations
    
    * Unittest for functions
    
    Co-authored-by: Mikko Kurkela <mikko.kurkela_c@sandvik.com>

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+// HTTP Callable: Create a new user record into firestore
+exports.addUser = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'Unauthenticated');
+  }
+
+  if (!data.uid) {
+    throw new functions.https.HttpsError('invalid-argument',
+      'Given data does not contain data.uid');
+  }
+
+  return admin.firestore().collection('users').doc(data.uid).set(data.data);
+});
+
+exports.deleteUser = functions.auth.user().onDelete(user => {
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
+});
---
+++ test/index.test.js
@@ -0,0 +1,45 @@
+
+const test = require('firebase-functions-test')();
+const admin = require('firebase-admin');
+const sinon = require('sinon');
+const chai = require('chai');
+
+const myFunctions = require('../index');
+const wrapped = test.wrap(myFunctions.addUser);
+
+const validRecord = {
+  uid: 'LoR1xY535HP6gNJNRBokMfhD8343',
+  data: {
+    githubUser: 'tester',
+    displayName: 'Test User',
+    email: 'test@test.fi',
+    photo: 'https://testurl.fi',
+    projects:[],
+    role: 1,
+  } 
+};
+
+
+describe('User records are stored into firestore',() => {
+  const firestoreStub = sinon.stub();
+  const collectionStub = sinon.stub();
+  const collectionArg = 'users';
+  const docStub = sinon.stub();
+  const setStub = sinon.stub();
+
+  Object.defineProperty(admin, 'firestore', { get: () => firestoreStub });
+  firestoreStub.returns({ collection: collectionStub });
+  collectionStub.withArgs(collectionArg).returns({ doc: docStub });
+  docStub.withArgs(validRecord.uid).returns({ set: setStub });
+  setStub.withArgs(validRecord.data).returns(true);
+
+  it('Valid user data from authenticated user is writtend to firestore', () => {
+
+    chai.assert.equal(wrapped(validRecord, {
+      auth: {
+        uid: 'LoR1xY535HP6gNJNRBokMfhD8343',
+        token: '123456'
+      },
+    }),true);
+  });
+});
