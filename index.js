ea09f1e3b3958cdc809267943d35165125596450
Mikko Kurkela <mikko.sahkoposti@gmail.com>
2020-04-21 21:44:48 +0300

    Fetch user data from firestore (#11)
    Co-authored-by: Mikko Kurkela <mikko.kurkela_c@sandvik.com>

--- index.js
+++ index.js
@@ -14,10 +14,25 @@ exports.addUser = functions.https.onCall((data, context) => {
       'Given data does not contain data.uid');
   }
 
+  console.log(data);
   return admin.firestore().collection('users').doc(data.uid).set(data.data);
 });
 
 exports.deleteUser = functions.auth.user().onDelete(user => {
   const doc = admin.firestore().collection('users').doc(user.uid);
   return doc.delete();
+});
+
+exports.getUser = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('unauthenticated', 'Unauthenticated');
+  }
+
+  if (!data.uid) {
+    throw new functions.https.HttpsError('invalid-argument',
+      'Given data does not contain data.uid');
+  }
+
+  console.log(data.uid);
+  return admin.firestore().collection('users').doc(data.uid).get();
 });
