76f80c1943892aff9d588598c161cd469d0e0cd8
Mikko Kurkela <mikko.sahkoposti@gmail.com>
2020-07-15 12:16:30 +0300

    Load projects for the app and pass it for pages in need (#163)
    Co-authored-by: Mikko Kurkela <mikko.kurkela@devolution.fi>

--- getProjects.js
+++ getProjects.js
@@ -1,13 +1,10 @@
 const admin = require('firebase-admin');
-const functions = require('firebase-functions');
 
 /**
  * List all registered users
  */
 module.exports = async(user, context) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError('unauthenticated', 'Unauthenticated');
-  }
+  console.log(context);
 
   return admin.firestore().collection('projects').get().then(projects => {
     const projectsData = projects.docs.map(doc => doc.data());
